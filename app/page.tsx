241fe0f4ca3682a434a3fc6b18d256b7d7785d96
Mary Ogedengbe <mary.ogedengbe@jwadelsolutions.net>
2024-02-05 08:21:22 -0800

    made some tailwind css changes

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,12 @@ import Link from 'next/link'
 
 const home = () => {
   return (
-    <div className="bg-lime-950 min-h-screen flex items-center justify-center">
-    <div className="bg-white p-6 rounded-lg shadow-md">
-      <h1 className="text-3xl font-bold text-gray-800 mb-4">Software Cost Estimation</h1>
-      <p className="text-gray-600 mb-4">Calculate the cost of your software project.</p>
+    <div className="bg-lime-950 min-h-screen flex  flex-col items-center justify-center">
+    <div className="bg-white p-8 rounded-lg shadow-md text-center">
+      <h1 className="text-4xl font-bold text-gray-800 mb-4">Software Cost Estimation</h1>
+      <p className="text-lg text-gray-600 mb-6">Calculate the cost of your software project.</p>
       <div className="flex justify-center">
-          <Link href="/start" className="px-4 py-2 bg-green-500 hover:bg-green-600 text-white rounded-md transition duration-300 text-center ">
+          <Link href="/start" className="px-6 py-3 bg-green-500 hover:bg-green-600 text-white rounded-md transition duration-300 text-lg text-center inline-block ">
             Start Estimation
           </Link>
         </div>
@@ -18,3 +18,5 @@ const home = () => {
 }
 
 export default home;
+
+
